e8aae6d24629c4c8ea1acfc7f31784bf1b0a8c80
Valeriy Loshchenkov <klarkisek@gmail.com>
2024-05-13 11:50:04 +0300

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,44 @@
+require('@nomicfoundation/hardhat-chai-matchers');
+require('@nomicfoundation/hardhat-ethers')
+require('hardhat-dependency-compiler');
+require('@typechain/hardhat')
+require('dotenv').config();
+
+module.exports = {
+    solidity: {
+      compilers: [
+        {
+          version: '0.8.24',
+          settings: {
+            optimizer: {
+              enabled: true,
+              runs: 1_000_000,
+            },
+            viaIR: true,
+          },
+        },
+      ]
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+    defaultNetwork: "hardhat",
+    namedAccounts: {
+      deployer: {
+          default: 0,
+      },
+    },
+    networks: {
+      hardhat: {
+        chainId: 31337,
+        blockGasLimit: 30000000,
+        gasPrice: 70_000_000_000,
+        mining:{
+          auto: true,
+          interval: 5000
+        }
+      },
+    },
+};
